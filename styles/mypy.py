5a1b8e71acac4691d63fb5c56509074ca3b00809
wmhst7 <wmhst@qq.com>
2021-03-18 21:09:18 +0800

    啊这

--- styles/mypy.py
+++ styles/mypy.py
@@ -1,18 +1,18 @@
-# # case 1 - missing typing
-# def case_1(**kwargs):
-#     print(kwargs)
-#
-#
-# # case 2 - re-assignment
-# def case_2() -> None:
-#     a = 0
-#     a = "Hello!"
-#     print(a)
-#
-#
-# # case 3 - wrong typing
-# def case_3(x: int) -> None:
-#     print(x)
-#
-#
-# case_3(1.0)
+# case 1 - missing typing
+def case_1(**kwargs):
+    print(kwargs)
+
+
+# case 2 - re-assignment
+def case_2() -> None:
+    a = 0
+    a = "Hello!"
+    print(a)
+
+
+# case 3 - wrong typing
+def case_3(x: int) -> None:
+    print(x)
+
+
+case_3(1.0)
