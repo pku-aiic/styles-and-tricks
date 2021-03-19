c9095cbffd1c1dec0ecbad399be925bb64ae6966
huangjiangyong <1800011062@pku.edu.cn>
2021-03-19 16:49:22 +0800

    fix

--- styles/mypy.py
+++ styles/mypy.py
@@ -1,11 +1,10 @@
 # case 1 - missing typing
-def case_1(**kwargs):
+def case_1(kwargs: float):
     print(kwargs)
 
 
 # case 2 - re-assignment
 def case_2() -> None:
-    a = 0
     a = "Hello!"
     print(a)
 
@@ -15,4 +14,4 @@ def case_3(x: int) -> None:
     print(x)
 
 
-case_3(1.0)
+case_3(1)
