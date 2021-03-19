7d105ca52bb482ad8c7fb19362c81dbe696b847c
EdgeOBerJY <68498923+EdgeOBerJY@users.noreply.github.com>
2021-03-19 17:43:30 +0800

    Update black.py

--- styles/black.py
+++ styles/black.py
@@ -1,8 +1,33 @@
 from typing import Any
 
 # case 1 - line length
-x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-     13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, ]
+x = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+]
 
 # case 2 - trailing comma
 y = [
@@ -27,8 +52,6 @@ def f1(
 
 
 def this_is_a_very_long_function_name(
-        my_fancy_arg1: Any,
-        my_fancy_arg2: Any,
-        my_fancy_arg3: Any,
+    my_fancy_arg1: Any, my_fancy_arg2: Any, my_fancy_arg3: Any
 ) -> None:
     pass
